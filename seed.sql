8a048a80e8cba8d05ec9b8456997d2b00da28099
seido <seid783@gmail.com>
2020-12-16 16:37:49 -0800

    day 3 book-app

---
+++ seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (title, authors, description, image_url, isbn) VALUES ('East Of Eden','John Steinbeck', 'A family tale of life in california', 'https://i.imgur.com/J5LVHEL.j', 9781440631320 );
+INSERT INTO books (title, authors, description, image_url, isbn) VALUES ('The Road','Cormac McCarthy', 'Afather and his son walk alone through burned America', 'https://i.imgur.com/J5LVHEL.j', 9780307267450 );
--- server.js
+++ server.js
@@ -19,25 +19,58 @@ client.on('error', (error) => console.error(error))
 
 app.use(cors());
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 
-app.get('/', sayingHello);
-app.get('/search', newSearch);
-app.post('/searches/new', searching);
+app.get('/', homePage);
+app.get('/hello', sayingHello);
+app.get('/searches/new', newSearch);
+app.get('/books/:id', bookDetails);
+app.post('/search', searching);
 
 
-function sayingHello(req, res){
-    res.render('pages/index.ejs' );
+function homePage(req, res) {
+    client.query('SELECT * FROM books')
+          .then(result => {
+              let savedBooks = result.rows;
+              
+               res.render('pages/index.ejs',{nsBooks: savedBooks}, {bookCount : savedBooks.length});
+               
+
+            })
+    
 }
+function sayingHello(req, res){
+    res.render('pages/index.ejs');
+}
+// function showError(req, res){
+//     res.status(404).render('pages/error.ejs');}
 
-function newSearch(req, res){
-    res.render('pages/searches/new.ejs' );
+
+function newSearch(req, res) {
+    res.render('pages/searches/new.ejs');
+}
+function bookDetails(req, res){
+    client.query('SELECT * FROM books WHERE id=$1', [req.params.id])
+          .then(result => {
+              let booksInDetail = result.rows[0]
+              res.render('pages/books/detail.ejs', {nsBooks : booksInDetail });
+          })
+
+    
+
 }
 
 function searching(req, res) {
     console.log(req.body);
+    const bookSearchURL = `https://www.googleapis.com/books/v1/volumes?q=${searchType}+${searchTerm}`
+    superagent.get(bookSearchURL)
+        .then(bookSearchReturn => {
+            const bookSearchArray = bookSearchReturn.body.items.map(instanceBook => new Book(instanceBook));
+
+            res.render('pages/searches/show', { bookSearchArray: bookSearchArray })
+        })
 
     let searchType;
     const searchTerm = req.body.search[0];
@@ -46,31 +79,15 @@ function searching(req, res) {
     } else {
         searchType = 'intitle';
     }
-    client.query('SELECT * FROM books WHERE title=$1', [req.body.search])
-        .then(book => {
-            if (book.rows.length !== 0) {
-                console.log(book.rows);
-                res.send(book.rows[0]);
-            } else {
-                const bookSearchURL = `https://www.googleapis.com/books/v1/volumes?q=${searchType}+${searchTerm}`
-                superagent.get(bookSearchURL)
-                    .then(bookSearchReturn => {
-                        const bookSearchArray = bookSearchReturn.body.items.map(instanceBook => new Book(instanceBook));
-                        const bookSql = `INSERT INTO books (title, authors, description, image_url, isbn) VALUES ($1, $2, $3, $4, $5)`;
-                        client.query(bookSql, [bookSearchArray.title, bookSearchArray.authors, bookSearchArray.description, bookSearchArray.image_url, bookSearchArray.isbn]);
-                        res.render('pages/searches/show', { bookSearchArray: bookSearchArray })
-                        console.log(client.query);
-                    })
-            }
-        })
+
+    
 }
 
 
 
-
-function Book(books){
+function Book(books) {
     this.title = books.volumeInfo.title;
-    this.authors = books.volumeInfo.authors;
+    this.authors = books.volumeInfo.authors &&books.volumeInfo.authors[0] || 'UNKNOWN';
     this.description = books.volumeInfo.description;
     /////// got this from the code review /////
     this.image_url = books.volumeInfo.imageLinks ? books.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
@@ -96,3 +113,13 @@ app.use('*', (request, response) => {
 client.connect().then(() => {
     app.listen(PORT, () => console.log(` app is listening ${PORT}`));
 })
+
+
+
+
+
+
+
+
+// const bookSql = `INSERT INTO books (title, authors, description, image_url, isbn) VALUES ($1, $2, $3, $4, $5)`;
+// client.query(bookSql, [bookSearchArray.title, bookSearchArray.authors, bookSearchArray.description, bookSearchArray.image_url, bookSearchArray.isbn]);
